74f864af336fcbf82817415c1570da868ff42d8a
Cuzzo <yahn007@gmail.com>
2012-10-08 16:00:21 -0400

    Added the Google Civic Address Block and where-do-I-vote view.

---
+++ tpls/google_civic-block-address.tpl.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file Google Civic Address Block Template.
+ */
+?>
+
+<h3>Find your polling place!</h3>
+<?php print $address_form; ?>
---
+++ tpls/google_civic-voterinfo.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file Google Civic Voterinfo Template.
+ */
+?>
+
+<h1><?php print $title; ?></h1>
+<?php if ($locations) : ?>
+  <?php foreach ($locations as $location) : ?>
+  <div class="google_civic-polling-location vcard">
+    <p class="fn org"><?php print $location->address->locationName; ?></p>
+    <div class="adr">
+      <p class="street-address"><?php print $location->address->line1; ?> <?php print $location->address->line2; ?> <?php print $location->address->line3; ?></p>
+      <div>
+        <p class="locality"><?php print $location->address->city; ?></p>, 
+        <p class="region"><?php print $location->address->state; ?></p> 
+        <p class="postal-code"><?php print $location->address->zip ?></p>
+        <p class="country-name"></p>
+      </div>
+    </div>
+  </div>
+  <div class="google_civic-polling-hours"><?php print $location->pollingHours; ?></div>
+  <div><?php print $location->map; ?></div>
+  <div>Election Info: <?php print $election_info_link; ?></div>
+  <?php endforeach; ?>
+<?php endif; ?>
+
