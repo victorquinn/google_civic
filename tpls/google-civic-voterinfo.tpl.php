6e661eb05aa635e5ea82ff432f74b2b89c17190f
Shai Sachs <ssachs@ngpvan.com>
2012-10-19 14:14:40 -0400

    Clean up tpl indentation.
    Signed-off-by: Shai Sachs <ssachs@ngpvan.com>

--- tpls/google-civic-voterinfo.tpl.php
+++ tpls/google-civic-voterinfo.tpl.php
@@ -13,30 +13,30 @@
     <div id="google-civic-voterinfo-gmap"><?php print $map; ?></div>
   <?php endif; ?>
 
-<?php if ($locations) : ?>
-  <div id="google-civic-voterinfo-polling-locations">
-    <?php print $locations; ?>
-  </div>
-<?php else: ?>
-  <p><?php print 
-    t('Sorry.  We are unable to find a polling location for your address.');
-  ?></p>
-<?php endif; ?>
+  <?php if ($locations) : ?>
+    <div id="google-civic-voterinfo-polling-locations">
+      <?php print $locations; ?>
+    </div>
+  <?php else: ?>
+    <p><?php print 
+      t('Sorry.  We are unable to find a polling location for your address.');
+    ?></p>
+  <?php endif; ?>
 
-<?php if ($early_locations) : ?>
-  <div id="google-civic-voterinfo-early-locations">
-    <?php print $early_locations; ?>
-  </div>
-<?php endif; ?>
+  <?php if ($early_locations) : ?>
+    <div id="google-civic-voterinfo-early-locations">
+      <?php print $early_locations; ?>
+    </div>
+  <?php endif; ?>
 
-<?php if ($election_info_link) : ?>
-  <div id="google-civic-voterinfo-election-info">
-    <?php print 
-      t('For more information, visit: !link', 
-          array('!link' => $election_info_link)); 
-    ?>
-  </div>
-<?php endif; ?>
+  <?php if ($election_info_link) : ?>
+    <div id="google-civic-voterinfo-election-info">
+      <?php print 
+        t('For more information, visit: !link', 
+            array('!link' => $election_info_link)); 
+      ?>
+    </div>
+  <?php endif; ?>
 
 </div>
 <div id="google-civic-address-form-div">
